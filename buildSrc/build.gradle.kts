7bd9922ac49e5124bccc07fc1d2052412ab3da9f
Plinio Alencar <plinioprojetos@hotmail.com>
2025-01-03 15:37:18 -0300

    add classpath

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation(libs.build.gradle)
     implementation(libs.ktlint.gradle)
     implementation(libs.ksp)
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
 }
 
 val compileKotlin: KotlinCompile by tasks
