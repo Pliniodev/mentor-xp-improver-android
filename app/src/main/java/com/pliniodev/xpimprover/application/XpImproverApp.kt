6a001a27d26552cf895386465d6b32c90693d779
Plinio Alencar <plinioprojetos@hotmail.com>
2025-01-03 13:24:43 -0300

    lint

--- app/src/main/java/com/pliniodev/xpimprover/application/XpImproverApp.kt
+++ app/src/main/java/com/pliniodev/xpimprover/application/XpImproverApp.kt
@@ -2,9 +2,9 @@ package com.pliniodev.xpimprover.application
 
 import android.app.Application
 import cafe.adriel.voyager.core.registry.ScreenRegistry
-import com.pliniodev.xpimprover.di.appModule
 import com.pliniodev.feature.di.featureModule
 import com.pliniodev.feature.di.featureScreenModule
+import com.pliniodev.xpimprover.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
--- feature/src/main/java/com/pliniodev/feature/di/StepDi.kt
+++ feature/src/main/java/com/pliniodev/feature/di/StepDi.kt
@@ -1,8 +1,8 @@
 package com.pliniodev.feature.di
 
 import cafe.adriel.voyager.core.registry.screenModule
-import com.pliniodev.xpimprover.navigation.SharedScreen
 import com.pliniodev.feature.FeatureHomeScreen
+import com.pliniodev.xpimprover.navigation.SharedScreen
 
 val featureScreenModule = screenModule {
     register<SharedScreen.FeatureHome> {
