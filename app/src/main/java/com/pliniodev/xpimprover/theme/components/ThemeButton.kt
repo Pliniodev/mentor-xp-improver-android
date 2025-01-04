defa4ccefb8509ef5f70134d60c2fc4ac8e6fe6a
Plinio Alencar <plinioprojetos@hotmail.com>
2025-01-04 09:36:25 -0300

    update button

--- app/src/main/java/com/pliniodev/xpimprover/theme/components/ThemeButton.kt
+++ app/src/main/java/com/pliniodev/xpimprover/theme/components/ThemeButton.kt
@@ -1,13 +1,10 @@
 package com.pliniodev.xpimprover.theme.components
 
-import androidx.compose.foundation.background
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 @Composable
@@ -18,13 +15,8 @@ public fun ThemeButton(
 ) {
     Button(
         onClick = onClick,
-        colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-        modifier = modifier.background(
-            brush = Brush.linearGradient(
-                listOf(Color.Blue, MaterialTheme.colorScheme.primaryContainer),
-            ),
-            shape = MaterialTheme.shapes.extraLarge,
-        ),
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+        modifier = modifier,
     ) {
         Text(text = text)
     }
