9ffc2be53e9faed0d593c63de9c19286e277222c
Plinio Alencar <plinioprojetos@hotmail.com>
2025-01-05 14:30:55 -0300

    add modules and update libs

---
+++ common/theme/build.gradle.kts
@@ -0,0 +1,35 @@
+import com.pliniodev.src.plugins.MainGradlewPlugin
+
+plugins {
+    `android-library`
+    `kotlin-android`
+    alias(libs.plugins.ksp)
+}
+
+apply<MainGradlewPlugin>()
+
+android {
+    namespace = "com.pliniodev.xpimprover.common.theme"
+}
+
+dependencies {
+
+    // compose
+    implementation(libs.bundles.compose)
+    implementation(platform(libs.compose.bom))
+
+    implementation(libs.coil)
+    implementation(libs.coil.compose)
+
+    // core
+    implementation(libs.bundles.ktx)
+    implementation(libs.material3)
+    implementation(libs.ksp)
+
+    // test
+    testImplementation(libs.junit)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.bundles.android.test)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+}
